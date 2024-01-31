4ab44ed2c5a3dd30805ea8d8f93476a882554b74
SamuelB348 <133508956+SamuelB348@users.noreply.github.com>
2024-01-31 19:01:51 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
+import Papa from 'papaparse';
 
+function submitForm() {
+  var selectedThemes = [];
+  var checkboxes = document.getElementsByName("theme");
+
+  checkboxes.forEach(function (checkbox) {
+    if (checkbox.checked) {
+      selectedThemes.push(checkbox.value);
+    }
+  });
+
+  Papa.parse("Indexation MQ03.csv", {
+    download: true,
+    header: true,
+    complete: function (results) {
+      var filteredData = results.data.filter(function (row) {
+        return selectedThemes.some(function (theme) {
+          return row[theme] === "1";
+        });
+      });
+
+      var column1Values = filteredData.map(function (row) {
+        return row["Colonne1"];
+      });
+
+      // Display the result on the webpage
+      console.log(column1Values);
+    },
+  });
+}
